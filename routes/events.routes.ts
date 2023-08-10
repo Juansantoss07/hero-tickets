431dd1c4eee55a5853530a17809c51e984495d9a
Juansantoss07 <juansantosscunha@gmail.com>
2023-08-09 23:44:34 -0300

    create a Event case

--- routes/events.routes.ts
+++ routes/events.routes.ts
@@ -1,15 +1,25 @@
 import { Router } from "express";
+import { EventRepositoryMongoose } from "../src/repositories/EventRepositoryMongoose";
+import { EventUseCase } from "../src/useCases/EventUseCase";
+import { EventController } from "../src/controllers/EventController";
 
 class EventRoutes {
     
     public router: Router;
+    private eventController: EventController;
 
     constructor() {
         this.router = Router();
+
+        const eventRepository = new EventRepositoryMongoose();
+        const eventUseCase = new EventUseCase(eventRepository);
+        this.eventController = new EventController(eventUseCase);
+        this.initRoutes();
     }
 
     initRoutes(){
-        this.router.post('/');
+        this.router.post('/',
+        this.eventController.create.bind(this.eventController));
     }
 }
 
